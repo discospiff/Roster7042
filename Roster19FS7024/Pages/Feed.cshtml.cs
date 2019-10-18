44a8b676b1d1f310880653970c6b1c6404d66ed6
jonesbr <brandan.jones@uc.edu>
2019-10-18 18:32:46 -0400

    Add JSON Feed

---
+++ Roster19FS7024/Pages/Feed.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Roster19FS7024.Pages
+{
+    public class FeedModel : PageModel
+    {
+        public JsonResult OnGet()
+        {
+            return new JsonResult(ShortcutRoster.allShortcuts);
+        }
+    }
+}
